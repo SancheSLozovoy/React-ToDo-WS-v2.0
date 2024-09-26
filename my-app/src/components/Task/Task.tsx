d183d91d6114a0f361e14dea1838b0b72adf483d
SanchesLozovoy <alex.lozovoy05@bk.ru>
2024-09-26 09:51:35 +0300

    fix : modified the task component to meet the new requirements

--- my-app/src/components/Task/Task.tsx
+++ my-app/src/components/Task/Task.tsx
@@ -1,35 +1,30 @@
 import React from 'react';
-import './Task.css'
+import './Task.css';
 import { TaskProps } from '../../types/Task.type';
 
-class Task extends React.Component<TaskProps> {
+const Task: React.FC<TaskProps> = ({ id, title, completed, onToggle, onDelete, onEdit }) => {
 
-    handleEdit = () => {
-        const newTitle = prompt("edit task title", this.props.title);
-        if(newTitle && newTitle.trim() !== ""){
-            this.props.onEdit(this.props.id, newTitle.trim())
+    const handleEdit = () => {
+        const newTitle = prompt("Edit task title", title);
+        if (newTitle && newTitle.trim() !== "") {
+            onEdit(id, newTitle.trim());
         }
-    }
+    };
 
-    render(){
+    const titleClass = completed ? 'task-title completed' : 'task-title';
 
-        const { title, completed, onToggle, onDelete, id} = this.props;
-
-        const titleClass = completed ? 'task-title completed' : 'task-title';
-
-        return(
-            <div className="task-container">
-                <div className="task-content">
-                    <span className={titleClass}>{title}</span>
-                    <div className="button-container">
-                        <input type="checkbox" checked={completed} onChange={() => onToggle(id)}></input>
-                        <button onClick={() => onDelete(id)}>Delete</button>
-                        <button className='change-button' onClick={this.handleEdit}>Change</button>
-                    </div>
+    return (
+        <div className="task-container">
+            <div className="task-content">
+                <span className={titleClass}>{title}</span>
+                <div className="button-container">
+                    <input type="checkbox" checked={completed} onChange={() => onToggle(id)} />
+                    <button onClick={() => onDelete(id)}>Delete</button>
+                    <button className="change-button" onClick={handleEdit}>Change</button>
                 </div>
             </div>
-        )
-    }
-}
+        </div>
+    );
+};
 
 export default Task;
